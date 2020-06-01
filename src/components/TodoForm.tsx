dbfc0ff1cc3d515a488fd422055fc59604c94fba
AnsilRsingh <65973056+AnsilRsingh@users.noreply.github.com>
2020-06-01 12:45:31 -0400

    removing formik adding yarn

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,45 @@
-import REACT, {Component} from 'react'
 import React from "react";
+import { Input, Button, Form, Switch} from 'antd';
 
-class TodoForm extends Component {
-    render() {
-        return (
-            <div>
+const layout = {
+    labelCol: { span: 8},
+    wrapperCol: {span: 16},
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16},
+};
 
-            </div>
-        )
-    }
+const TodoForm = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (value: any) => {
+        console.log("values");
+    };
+
+    const onReset = () => {
+        form.resetFields();
+    };
+
+    // @ts-ignore
+    return (
+        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+            <Form.Item name="title" label="title" rules={[{ required: true}]}>
+                <Input.TextArea />
+            </Form.Item>
+            <Form.Item name="complete" label="Complete" valuePropName="checked" initialValue={false}>
+                <Switch />
+            </Form.Item>
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                <Button htmlType="button" onClick={onReset}>
+                Reset
+                </Button>
+
+            </Form.Item>
+
+        </Form>
+    );
 }
+ export default TodoForm;
