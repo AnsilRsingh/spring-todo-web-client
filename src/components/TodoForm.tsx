db3275a1db51df453ca3291e386ec9d1c6d25785
AnsilRsingh <65973056+AnsilRsingh@users.noreply.github.com>
2020-06-01 17:16:56 -0400

    updates to description

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,36 +1,42 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Input, Button, Form, Switch } from 'antd';
-const layout = {
+import styles from './styles.module.css';
+import {postTodo} from "../service/todoService";
+const layout =  {
     labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    wrapperCol: { span: 8 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
-};
-const TodoForm = () => {
+    wrapperCol: { offset: 8, span: 8 },
+}
+interface TodoFormInterface {
+    reload(): void;
+}
+const TodoForm = (props: TodoFormInterface) => {
     const [form] = Form.useForm();
     const onFinish = (values: any) => {
         console.log(values);
+        postTodo(values).then(() => props.reload());
     };
     const onReset = () => {
         form.resetFields();
     };
     return (
-        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-            <Form.Item name="title" label="Title" rules={[{required: true}]}>
+        <Form {...layout} form={form} name = "control-hooks" onFinish= {onFinish}>
+            <Form.Item name="title" label="Title" rules={[{ required: true}]}>
                 <Input />
             </Form.Item>
-            <Form.Item name="description" label="Description" rules={[{required: true}]}>
+            <Form.Item name="description" label="Description" rules={[{ required: true}]} >
                 <Input.TextArea />
             </Form.Item>
             <Form.Item name="complete" label="Complete" valuePropName="checked" initialValue={false}>
                 <Switch />
             </Form.Item>
             <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType ='submit'>
                     Submit
                 </Button>
-                <Button htmlType="button" onClick={onReset}>
+                <Button htmlType="button" onClick={onReset} className={styles.button}>
                     Reset
                 </Button>
             </Form.Item>
