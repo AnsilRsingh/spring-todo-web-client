f1772bb725986ff2c2b91252fa50fdb69cdc81a9
AnsilRsingh <65973056+AnsilRsingh@users.noreply.github.com>
2020-06-01 13:50:45 -0400

    fixed todo form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,29 +1,26 @@
-import React from "react";
-import { Input, Button, Form, Switch} from 'antd';
-
+import React, {Component} from 'react';
+import { Input, Button, Form, Switch } from 'antd';
 const layout = {
-    labelCol: { span: 8},
-    wrapperCol: {span: 16},
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16},
+    wrapperCol: { offset: 8, span: 16 },
 };
-
 const TodoForm = () => {
     const [form] = Form.useForm();
-
-    const onFinish = (value: any) => {
-        console.log("values");
+    const onFinish = (values: any) => {
+        console.log(values);
     };
-
     const onReset = () => {
         form.resetFields();
     };
-
-    // @ts-ignore
     return (
         <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-            <Form.Item name="title" label="title" rules={[{ required: true}]}>
+            <Form.Item name="title" label="Title" rules={[{required: true}]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name="description" label="Description" rules={[{required: true}]}>
                 <Input.TextArea />
             </Form.Item>
             <Form.Item name="complete" label="Complete" valuePropName="checked" initialValue={false}>
@@ -34,12 +31,10 @@ const TodoForm = () => {
                     Submit
                 </Button>
                 <Button htmlType="button" onClick={onReset}>
-                Reset
+                    Reset
                 </Button>
-
             </Form.Item>
-
         </Form>
     );
 }
- export default TodoForm;
+export default TodoForm;
