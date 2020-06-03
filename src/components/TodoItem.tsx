d64f6d8a8a967b84c350396697b58ee8e9a4129c
AnsilRsingh <65973056+AnsilRsingh@users.noreply.github.com>
2020-06-03 10:58:43 -0400

    add modal

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,34 +1,90 @@
 import {Component} from "react";
-import {Space, Card, Checkbox, Col} from "antd";
+import {Space, Card, Checkbox, Col, Button, Modal} from "antd";
 import React from "react";
 import Todo from "../service/Todo";
 import styles from "./styles.module.css";
+import {deleteTodoById} from "../service/todoService";
+import TodoUpdateModal from "./TodoUpdateModal";
 
 interface TodoProp {
-
     todo: Todo;
+    reload(): void;
 }
-class TodoItem extends Component<TodoProp> {
+
+interface TodoState {
+    modalVisible: boolean;
+    updateModalVisible: boolean;
+}
+class TodoItem extends Component<TodoProp, TodoState> {
     constructor(props: TodoProp) {
         super(props);
+        this.state = {
+            modalVisible: false,
+            updateModalVisible: false,
+
+        }
+    }
+
+    showModal = () => {
+        this.setState({
+            modalVisible: true,
+        });
+    };
+
+    handleOk = (e: any) => {
+        console.log(e);
+        this.setState({
+            modalVisible: false,
+        });
+    };
+
+    handleUpdate = () => {
+        this.setState({
+            updateModalVisible: true
+        })
+    }
+    handleCancel = () => {
+        this.setState({
+            modalVisible: false,
+            updateModalVisible: false
+        });
+    };
+
+    deleteTodo = () => {
+        if (this.props.todo.id != null && this.props.todo.complete) {
+            deleteTodoById(this.props.todo.id).then(() => this.props.reload());
+        } else {
+            this.showModal();
+        }
     }
 
     render() {
-
-        // @ts-ignore
         return (
-            <Col className="gutter-ro" span={6}>
-                <Card title = {this.props.todo.title} className={styles.todoCard}>
-                    <p>description </p>
-                        <p> {this.props.todo.description}</p>
-                            <p> Complete? :<Checkbox defaultChecked={this.props.todo.complete} disabled/></p>
-                            </Card>
-                                </Col>
+            <Col className="gutter-ro" span={6} >
+                <Card title={this.props.todo.title} className={styles.todoCard}>
+                    <p>Description:</p>
+                    <p>{this.props.todo.description}</p>
+                    <p>Complete: <Checkbox disabled checked={this.props.todo.complete}/></p>
+                    <Button type="primary" onClick={this.handleUpdate}>Update</Button>
+                    <Button type="primary" danger onClick={this.deleteTodo}>
+                        Delete
+                    </Button>
+                    <Modal
+                        title="Delete Todo"
+                        visible={this.state.modalVisible}
+                        onOk={this.handleOk}
+                        onCancel={this.handleCancel}
+                    >
+                        <p className={styles.dangerDeleteTodo}>In order to delete a todo, it MUST be completed!</p>
+                    </Modal>
+                    <TodoUpdateModal visible={this.state.updateModalVisible}
+                                     todo={this.props.todo}
+                                     onCancel={this.handleCancel}
+                                     reload={this.props.reload}/>
+                </Card>
+            </Col>
+        );
+    }
+}
 
-                                    );
-
-                                    }
-
-                                    }
-
-                                    export default TodoItem;
+export default TodoItem;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,36 +5,41 @@ import TodoItem from "./TodoItem";
 import {Row} from "antd";
 import TodoForm from "./TodoForm";
 import styles from './styles.module.css';
+
 interface TodoListState {
     todos: Todo[];
     loading: boolean;
 }
+
 class TodoList extends Component<any, TodoListState> {
     state = {
         todos: [],
         loading: true
     }
+
     async componentDidMount() {
         await this.loadPage();
     }
+
     loadPage = async () => {
         let todos = await getTodos();
         this.setState({todos, loading: false});
     }
+
     render() {
         return (
             <div className={styles.todoBody}>
                 <h2>This is a todo list</h2>
                 {this.state.loading ? (
                     <>
-                        <h2>this is loading</h2>
+                        <h2>Loading...</h2>
                     </>
                 ) : (
                     <>
                         <TodoForm reload={this.loadPage}/>
                         <Row className={styles.todoRows}>
                             {this.state.todos.map((todo: Todo) =>
-                                <TodoItem key={todo.id} todo={todo}/>
+                                <TodoItem key={todo.id} todo={todo} reload={this.loadPage}/>
                             )}
                         </Row>
                     </>
@@ -43,4 +48,5 @@ class TodoList extends Component<any, TodoListState> {
         );
     }
 }
+
 export default TodoList;
